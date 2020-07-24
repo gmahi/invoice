ab20631053a2407897729336c802a6382b3e214b
Gulla Mahendra <mahendra.gulla@ibsolution.de>
2020-07-24 14:48:53 +0000

    Internationalization

--- controller/App.controller.js
+++ controller/App.controller.js
@@ -1,8 +1,9 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
 	"sap/m/MessageToast",
-	"sap/ui/model/json/JSONModel"
-], function (Controller, MessageToast, JSONModel) {
+	"sap/ui/model/json/JSONModel",
+	"sap/ui/model/resource/ResourceModel"
+], function (Controller, MessageToast, JSONModel, ResourceModel) {
 	"use strict";
 	return Controller.extend("search.Invoice.controller.App", {
 		onInit: function () {
@@ -15,15 +16,13 @@ sap.ui.define([
 				}
 			};
 
-			var oModel = new JSONModel({
-				invInput: {
-					invNumber: "",
-					invType: "",
-					invCompanyCode: ""
-				}
+			var oModel = new JSONModel(invInput);
+			this.getView().setModel(oModel);
+			var i18nModel = new ResourceModel({
+				bundleName: "search.Invoice.i18n.i18n"
 			});
 
-			this.getView().setModel(oModel);
+			this.getView().setModel(i18nModel, 'i18n');
 		},
 		onSearchInvoices: function () {
 
