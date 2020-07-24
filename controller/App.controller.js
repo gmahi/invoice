7797360a946b6866fb2b3accf899aaff95930f54
Gulla Mahendra <mahendra.gulla@ibsolution.de>
2020-07-24 14:22:30 +0000

    model

--- controller/App.controller.js
+++ controller/App.controller.js
@@ -1,10 +1,31 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"sap/m/MessageToast"
-], function (Controller, MessageToast) {
+	"sap/m/MessageToast",
+	"sap/ui/model/json/JSONModel"
+], function (Controller, MessageToast, JSONModel) {
 	"use strict";
 	return Controller.extend("search.Invoice.controller.App", {
-		onSearchInvoice: function () {
+		onInit: function () {
+			// Set data model on view.
+			var invInput = {
+				invInput: {
+					invNumber: "",
+					invType: "",
+					invCompanyCode: ""
+				}
+			};
+
+			var oModel = new JSONModel({
+				invInput: {
+					invNumber: "",
+					invType: "",
+					invCompanyCode: ""
+				}
+			});
+
+			this.getView().setModel(oModel);
+		},
+		onSearchInvoices: function () {
 
 			MessageToast.show("Searching Invoice's");
 		}
